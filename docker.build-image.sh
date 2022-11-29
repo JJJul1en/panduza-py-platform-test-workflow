4d55e348829f0951082b6876f3b9f6cb48e3743a
JJJul1en <50075377+JJJul1en@users.noreply.github.com>
2022-11-29 12:00:23 +0100

    Workflows build docker images (#20)
    * workflows test
    
    * workflow in right folder
    
    * update file
    
    * cache removed
    
    * if removed
    
    * print meta output
    
    * push du dockerfile modifié
    
    * push du dockerfile modifié
    
    * script plug in ajout anti slash
    
    * ajout d'antislash
    
    * from 4 to 2 workflows
    
    * un seul build and push
    
    * erratum
    
    * trying matrix keyword
    
    * v2 instead of v3
    
    * uncommented tags line
    
    * removed "latest" from image in matrix
    
    * added PZA_PY_PLATFORM_MODE arg variable
    
    * build script added
    
    * build script added
    
    * test of workflows
    
    * Using REPO variable
    
    * Change REPO to IMAGE_NAME
    
    * env.repo and not repo alone
    
    * test
    
    * test
    
    * context availability problem
    
    * test context
    
    * trying generic
    
    Co-authored-by: JJJul1en <julien.mallet@elsys-design.com>
    Co-authored-by: Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>

--- docker.build-image.sh
+++ docker.build-image.sh
@@ -2,7 +2,8 @@
 
 if [ $# -eq 0 ]
 then
-    docker build -t local/panduza-py-platform:latest .
+    docker build -t local/panduza-py-platform:latest .  --build-arg PZA_PY_PLATFORM_MODE=prod
 else
-    docker build -t local/panduza-py-platform-dev:latest .
+    docker build -t local/panduza-py-platform-dev:latest . --build-arg PZA_PY_PLATFORM_MODE=dev
 fi
+
